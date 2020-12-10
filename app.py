d77b5f0eb17acb9de6d7833017a7ab583ed1fddc
Michael Skripkin <m.skripkin@alpha-safety.kz>
2020-12-10 11:29:22 +0600

    update: dev group

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from values import send_message
+from values import send_message, get_chats
 
 from flask import Flask, request, jsonify
 app = Flask(__name__)
@@ -8,10 +8,19 @@ app = Flask(__name__)
 def merge_request():
     data = request.get_json()
     print(data)
-    text = f"{data['user']['name']} под username {data['user']['username']} сделал реквест на бранч {data['object_attributes']['target_branch']}"
+    text = f"tracking_front: {data['user']['name']} под username {data['user']['username']} сделал реквест на бранч {data['object_attributes']['target_branch']}"
     send_message(text)
     return text
 
 
+@app.route('/get_updates', methods=['GET'])
+def get_updates():
+    data = request.get_data()
+    get_chats()
+    print(data)
+
+    return 'asd'
+
+
 if __name__ == '__main__':
-    app.run(threaded=True, port=6001)
+    app.run(threaded=True, port=5000)
--- values.py
+++ values.py
@@ -1,9 +1,17 @@
 from urllib import request, parse
 
 bot_api = '1331155386:AAEKWdb-XdW0-4ykTRgel4byIIAZ1bO6kvI'
+dev_group_chat_id = '-351804047'
+channel_chat_id = '-1001132409362'
 
 
 def send_message(text: str):
     parsed_text = parse.quote(text)
-    req = request.Request(f'https://api.telegram.org/bot{bot_api}/sendMessage?chat_id=-1001132409362&text={parsed_text}')
+    req = request.Request(f'https://api.telegram.org/bot{bot_api}/sendMessage?chat_id={dev_group_chat_id}&text={parsed_text}')
     request.urlopen(req)
+
+
+def get_chats():
+    req = request.Request(f'https://api.telegram.org/bot{bot_api}/getUpdates')
+    resp = request.urlopen(req)
+    print(resp.read())
