76cb0dcea5d2348bd9b462230e09f4ab9e4e6150
Michael Skripkin <m.skripkin@alpha-safety.kz>
2020-12-11 10:12:45 +0600

    update: push, deployment events added

--- app.py
+++ app.py
@@ -1,28 +1,57 @@
 from values import send_message, get_chats
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def welcome_page():
+    return render_template('welcome-page.html')
 
 
 @app.route('/merge_request', methods=['POST'])
 def merge_request():
     data = request.get_json()
-    print(data)
-    text = f"tracking_front: {data['user']['name']}({data['user']['username']}) сделал реквест на бранч {data['object_attributes']['target_branch']}" \
-           f", Последний коммит: {data['object_attributes']['last_commit']['message']}"
+    text = f"tracking_front: {data['user']['name']}({data['user']['username']}) сделал реквест на бранч " \
+           f"{data['object_attributes']['target_branch']}, Последний коммит: " \
+           f"{data['object_attributes']['last_commit']['message']}"
     if data['object_attributes']['state'] == 'opened':
         send_message(text)
 
     return text
 
 
+@app.route('/push_event', methods=['POST'])
+def push_event():
+    data = request.get_json()
+    text = f"{data['user_name']}({data['user_username']}) сделал пуш в реп, Количество коммитов: " \
+           f"{data['total_commits_count']}"
+    if data['object_kind'] == 'push':
+        send_message(text)
+
+    return text
+
+
+@app.route('/deployment_event', methods=['POST'])
+def deployment_event():
+    data = request.get_json()
+    text = f"Деплой({data['commit_title']}) "
+    if data['status'] == 'running':
+        text += 'начался'
+    elif data['status'] == 'success':
+        text += 'прошел)'
+    elif data['status'] == 'failed':
+        text += 'упал('
+    elif data['status'] == 'canceled':
+        text += 'отменен'
+    send_message(text)
+
+    return text
+
+
 @app.route('/get_updates', methods=['GET'])
 def get_updates():
-    data = request.get_data()
-    get_chats()
-    print(data)
-
-    return 'asd'
+    return get_chats()
 
 
 if __name__ == '__main__':
--- values.py
+++ values.py
@@ -1,17 +1,22 @@
 from urllib import request, parse
+import json
 
 bot_api = '1331155386:AAEKWdb-XdW0-4ykTRgel4byIIAZ1bO6kvI'
 dev_group_chat_id = '-351804047'
 channel_chat_id = '-1001132409362'
+osman_chat_id = '300216268'
+
+CURRENT_CHAT_ID = channel_chat_id
 
 
 def send_message(text: str):
     parsed_text = parse.quote(text)
-    req = request.Request(f'https://api.telegram.org/bot{bot_api}/sendMessage?chat_id={channel_chat_id}&text={parsed_text}')
+    req = request.Request(f'https://api.telegram.org/bot{bot_api}/sendMessage?chat_id={CURRENT_CHAT_ID}&text={parsed_text}')
     request.urlopen(req)
 
 
 def get_chats():
     req = request.Request(f'https://api.telegram.org/bot{bot_api}/getUpdates')
     resp = request.urlopen(req)
-    print(resp.read())
+
+    return json.loads(resp.read())
